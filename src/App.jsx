d4c3fd74559d1b4cf5e8050cb5c3a49f2c77e654
Danae02 <danae.vandermeer@student.hu.nl>
2026-01-09 20:08:35 +0100

    klaar?

--- src/App.jsx
+++ src/App.jsx
@@ -5,38 +5,63 @@ import VeeIndustrieLongread from "./components/VeeIndustrieLongread.jsx";
 import InterviewsPage from "./components/InterviewsPage.jsx";
 import KunstKennisCultuurPage from "./components/KunstKennisCultuurPage.jsx";
 import Navigation from "./components/Navigation.jsx";
-import WatKanIkDoenPage from "./components/WatKanIkDoenPage.jsx";
 
-// Component om naar boven te scrollen bij route verandering
-function ScrollToTop() {
-    const { pathname } = useLocation();
+// Component om scroll gedrag te beheren
+function ScrollManager() {
+    const { pathname, hash, key } = useLocation();
 
     useEffect(() => {
-        // Scroll naar boven bij elke route verandering
-        window.scrollTo(0, 0);
+        // Log voor debugging
+        console.log('Location changed:', { pathname, hash, key });
 
-        // Extra: verwijder hash uit URL als die er is
-        if (window.location.hash) {
-            // Verwijder hash zonder pagina te herladen
-            window.history.replaceState(null, null, ' ');
+        // Als we op de hoofdpagina zijn EN er is een hash
+        if (pathname === '/' && hash) {
+            console.log('Hash detected on homepage:', hash);
+
+            // Haal het hash ID op zonder de #
+            const elementId = hash.replace('#', '');
+
+            // Wacht tot de pagina volledig geladen is
+            const timer = setTimeout(() => {
+                const element = document.getElementById(elementId);
+                if (element) {
+                    console.log('Scrolling to element:', elementId);
+                    element.scrollIntoView({ behavior: 'smooth' });
+                } else {
+                    console.log('Element not found, scrolling to top');
+                    window.scrollTo({ top: 0, behavior: 'smooth' });
+                }
+            }, 500); // Langere timeout voor zekerheid
+
+            return () => clearTimeout(timer);
         }
-    }, [pathname]);
+        // Als we op de hoofdpagina zijn zonder hash
+        else if (pathname === '/' && !hash) {
+            console.log('No hash, scrolling to top');
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        }
+        // Voor andere routes
+        else {
+            console.log('Different route, scrolling to top');
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        }
+    }, [pathname, hash, key]);
 
     return null;
 }
 
-
 function App() {
     return (
         <Router>
-            <ScrollToTop />
+            <ScrollManager />
             <Navigation />
             <Routes>
                 <Route path="/" element={<VeeIndustrieLongread />} />
                 <Route path="/interviews" element={<InterviewsPage />} />
                 <Route path="/interviews/:id" element={<InterviewsPage />} />
                 <Route path="/kunst-kennis-cultuur" element={<KunstKennisCultuurPage />} />
-                <Route path="/wat-kan-ik-doen" element={<WatKanIkDoenPage />} />
+                {/* Optioneel: redirect oudere hashes */}
+                <Route path="*" element={<VeeIndustrieLongread />} />
             </Routes>
         </Router>
     );
