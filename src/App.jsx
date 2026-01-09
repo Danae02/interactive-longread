03fc79b28878a2bad6e04b9e7ebfe3b0861eef64
Danae02 <danae.vandermeer@student.hu.nl>
2026-01-09 12:21:41 +0100

    klaar?

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
 // App.jsx
-import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import VeeIndustrieLongread from "./components/VeeIndustrieLongread.jsx";
 import InterviewsPage from "./components/InterviewsPage.jsx";
 import KunstKennisCultuurPage from "./components/KunstKennisCultuurPage.jsx";
 import Navigation from "./components/Navigation.jsx";
 import WatKanIkDoenPage from "./components/WatKanIkDoenPage.jsx";
 
+// Component om naar boven te scrollen bij route verandering
+function ScrollToTop() {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        // Scroll naar boven bij elke route verandering
+        window.scrollTo(0, 0);
+
+        // Extra: verwijder hash uit URL als die er is
+        if (window.location.hash) {
+            // Verwijder hash zonder pagina te herladen
+            window.history.replaceState(null, null, ' ');
+        }
+    }, [pathname]);
+
+    return null;
+}
+
 function App() {
     return (
         <Router>
+            <ScrollToTop />
             <Navigation />
             <Routes>
                 <Route path="/" element={<VeeIndustrieLongread />} />
